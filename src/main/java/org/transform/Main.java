4b780893b1328e48779e9015f0d245bdf52e8b0b
Allan Fang <32990185+allanfang1@users.noreply.github.com>
2024-07-15 19:38:56 +0200

    upgrade to jena 5.0.0

--- src/main/java/org/transform/Main.java
+++ src/main/java/org/transform/Main.java
@@ -1,19 +1,25 @@
 package org.transform;
 
+import com.apicatalog.jsonld.JsonLd;
+import com.apicatalog.jsonld.JsonLdError;
 import org.apache.commons.io.FileUtils;
 import org.apache.jena.rdf.model.InfModel;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.reasoner.Reasoner;
 import org.apache.jena.reasoner.ReasonerRegistry;
+import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
 import org.apache.jena.riot.RDFWriter;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Model defaultModel = ModelFactory.createDefaultModel();
         System.out.println("reading");
         FileUtils.iterateFiles(new File("./"), new String[]{"owl"}, false).forEachRemaining(file -> {
@@ -29,9 +35,10 @@ public class Main {
         reasoner = reasoner.bindSchema(schema);
 
         InfModel infmodel = ModelFactory.createInfModel(reasoner, defaultModel);
+
+        System.out.println("writing");
         new File("./out").mkdirs();
-        RDFWriter.create(infmodel).format(RDFFormat.JSONLD_EXPAND_PRETTY).output("./out/data_infer_exp.jsonld");
-        RDFWriter.create(infmodel).format(RDFFormat.JSONLD_FLATTEN_PRETTY).output("./out/data_infer_flat.jsonld");
-        RDFWriter.create(infmodel).format(RDFFormat.NT).output("./out/data_infer.nt");
+        RDFDataMgr.write(new FileOutputStream("./out/data_infer_exp.jsonld"), infmodel, Lang.JSONLD); //TODO: flatten
+        RDFDataMgr.write(new FileOutputStream("./out/data_infer.nt"), infmodel, Lang.NT);
     }
 }
